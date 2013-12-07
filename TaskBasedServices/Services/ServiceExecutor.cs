f8941f05763b9db5da1c4716c2533f29d63806a2
stevenh77 <stevenhollidge@hotmail.com>
2013-12-07 12:27:17 +0100

    initial commit

---
+++ TaskBasedServices/Services/ServiceExecutor.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Net;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace TaskBasedServices.Services
+{
+    public class ServiceExecutor
+    {
+        public void Get<TDto>(Action<TDto> callback)
+        {
+            var client = new WebClient();
+            client.DownloadStringCompleted += (sender, args) =>
+            {
+                if (args.Error != null)
+                    throw new Exception(args.Error.Message);
+                else if (args.Cancelled)
+                    Console.WriteLine("Service call cancelled");
+                else
+                {
+                    var dto = JsonConvert.DeserializeObject<TDto>(args.Result);
+                    callback(dto);
+                }
+            };
+
+            const string baseAddress = "http://localhost:8080/Services/";
+            var serviceName = typeof(TDto).Name + ".ashx";
+            client.DownloadStringAsync(new Uri(baseAddress + serviceName, UriKind.Absolute));
+        }
+
+        public Task<TDto> GetAsTask<TDto>()
+        {
+            var tcs = new TaskCompletionSource<TDto>();
+            var client = new WebClient();
+
+            client.DownloadStringCompleted += (sender, args) =>
+            {
+                if (args.Error != null)
+                    tcs.SetException(args.Error);
+                else if (args.Cancelled)
+                    tcs.SetCanceled();
+                else
+                {
+                    var dto = JsonConvert.DeserializeObject<TDto>(args.Result);
+                    tcs.SetResult(dto);
+                }
+            };
+
+            const string baseAddress = "http://localhost:8080/Services/";
+            var serviceName = typeof(TDto).Name + ".ashx";
+            client.DownloadStringAsync(new Uri(baseAddress + serviceName, UriKind.Absolute));
+            return tcs.Task;
+        }
+    }
+}
---
+++ TaskBasedServices/ViewModels/DealParameters.cs
@@ -0,0 +1,47 @@
+﻿using System.Threading.Tasks;
+using System.Windows.Input;
+using AutoMapper;
+using TaskBasedServices.Common;
+using TaskBasedServices.Models;
+using TaskBasedServices.Services;
+
+namespace TaskBasedServices.ViewModels
+{
+    public class DealParameters
+    {
+        private readonly ServiceExecutor services;
+
+        public DealParameters()
+        {
+            services = new ServiceExecutor();
+            BuildDealCommand = new RelayCommand(OnBuildCommand);
+        }
+
+        public MoneyMarketRate MoneyMarketRate { get; set; }
+        public InvestmentBoundaries InvestmentBoundaries { get; set; }
+        public TradingDate TradingDate { get; set; }
+        public Spot Spot { get; set; }
+
+        public ICommand BuildDealCommand { get; private set; }
+
+        private void OnBuildCommand()
+        {
+            var tasks = new Task[]
+            {
+                services.GetAsTask<DTOs.MoneyMarketRate>()
+                        .ContinueWith(t => MoneyMarketRate = Mapper.Map<DTOs.MoneyMarketRate, MoneyMarketRate>(t.Result)),
+
+                services.GetAsTask<DTOs.InvestmentBoundaries>()
+                        .ContinueWith(t => InvestmentBoundaries = Mapper.Map<DTOs.InvestmentBoundaries, InvestmentBoundaries>(t.Result)),
+
+                services.GetAsTask<DTOs.TradingDate>()
+                        .ContinueWith(t => TradingDate = Mapper.Map<DTOs.TradingDate, TradingDate>(t.Result)),
+            };
+
+            Task.Factory.ContinueWhenAll(
+                tasks,
+                ts => services.GetAsTask<DTOs.Spot>()
+                                .ContinueWith(t => Spot = Mapper.Map<DTOs.Spot, Spot>(t.Result)));
+        }
+    }
+}
